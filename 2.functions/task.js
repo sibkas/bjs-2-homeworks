30dd0a92de0df4dbb4efc56f59b106809f3d0445
sibkas <106993347+sibkas@users.noreply.github.com>
2025-11-20 12:17:06 +0700

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -11,9 +11,7 @@ let avg = Number((sum / arr.length).toFixed(2));
 return { min: min, max: max, avg: avg };
 }
 
-console.log(getArrayParams(-99, 99, 10)); // { min: -99, max: 99, avg: 3.33 }
-console.log(getArrayParams(1, 2, 3, -100, 10)); // { min: -100, max: 10, avg: -16.80 }
-console.log(getArrayParams(5)); // { min: 5, max: 5, avg: 5 }
+
 
 //Задача 2
 
