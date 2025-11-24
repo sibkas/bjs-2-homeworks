8a9cdd6d947c3e4f7045687acd253714f465e1da
sibkas <106993347+sibkas@users.noreply.github.com>
2025-11-24 13:44:22 +0700

    Update task.js
    выполнение задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
+//задача1
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
+
+//задача2
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filtered = users.filter(user => user.gender === gender);
+
+  if (filtered.length === 0) {
+    return 0;
+  }
+
+  const ages = filtered.map(user => user.age);
+
+  const average = ages.reduce((sum, age) => sum + age, 0) / ages.length;
+
+  return average;
 }
