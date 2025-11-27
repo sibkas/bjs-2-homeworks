31a2e5ec0d5ba951fbda897387de6c597167f978
sibkas <106993347+sibkas@users.noreply.github.com>
2025-11-27 18:01:46 +0700

    Update task.js
    решение дз

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+// Создание студентов
+const student1 = new Student("Алиса", "Ж", 20);
+const student2 = new Student("Борис", "М", 21);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+// Установка предмета
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+// Установка предмета студенту
+student1.setSubject("Математика");
 
-Student.prototype.exclude = function (reason) {
-  
-}
+// Добавление оценок
+Student.prototype.addMarks = function(...marks) {
+  if (!this.marks) {
+    // студент исключён, marks удалены
+    return;
+  }
+  this.marks.push(...marks);
+};
+
+// Получение среднего балла
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
+
+// Исключение студента
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
