df65b90054545b20d78e0a6249c3c2e203fafbfb
sibkas <106993347+sibkas@users.noreply.github.com>
2025-12-04 11:42:41 +0700

    Update task.js
    решение

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿//Задание 1
+function parseCount(value) {
+  let parsedValue = Number.parseFloat(value);
+
+  if (Number.isNaN(parsedValue)) {
+    throw new Error("Невалидное значение");
+
+  } else return parsedValue;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+//Задание 2
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    let p = this.perimeter / 2;
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
