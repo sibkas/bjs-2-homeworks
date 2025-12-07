99d284f278fa0471e77f15edc35fa07ccd21211f
sibkas <106993347+sibkas@users.noreply.github.com>
2025-12-07 13:28:34 +0700

    Update task.js
    решение

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,57 @@
-//Задача № 1
+//Задание 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.result);
+      return "Из кеша: " + objectInCache.result;
+    }
+    let result = func(...args);
+    cache.push({
+      hash,
+      value: result
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;
+
+  }
+  return wrapper;
 }
 
-//Задача № 2
+//Задание 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (!timeoutId) {
+      func.apply(this, args);
+      wrapper.count++;
+
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+      }, delay);
+
+    } else {
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+        wrapper.count++;
+        timeoutId = null;
+      }, delay);
+    }
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
